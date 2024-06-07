85e7ce118cc7005e48b3637153ff3e354890da5b
Kacper Kurowski <Kacper Kurowski@LAPTOP-RPJJ55SM>
2024-06-07 11:49:22 +0200

    Dodaj pliki projektów.

---
+++ AsmSzukaj/Asmsource.asm
@@ -0,0 +1,41 @@
+.model flat
+.data
+.code
+
+public AsmStart
+
+AsmStart proc
+   	; w  modelu flat rejestry CS,DS i ES maja ta sama wartosc 
+	; a DS:[SI] i ES:[DI] bedziemy porownywac
+
+	mov		edx, [esp+16]			; pobierz czwarty argument - ile znaków w linii do przeszukania
+	mov		esi, [esp+12]			; pobierz trzeci argument - wsk. na linie 
+	mov		edi, [esp+8]			; pobierz drugi argument - wsk. na s³owo ktore szukamy  
+	mov		ecx, [esp+4]			; pobierz pierwszy argument - wielkosc slowa szukanego
+
+	mov		ebx, esi			    ; pobierz trzeci argument - wsk. na linie 
+   	xor		eax, eax			    ; wyzeruj 
+	add		edx, esi			    ; do testowania wskaŸnika koñca linii
+
+PETLA:
+	mov		esi, ebx				; przywroc poprawna wartosc rejestru esi (indeks w stringu, w ktorym trzeba szukac)
+	mov		edi, [esp+8]			; przywroc poprawna wartosc rejestru edi (poczatek ciagu znakow, ktorego szukac w stringu)
+	inc		ebx						; zwieksz wskaznik, ktory bedzie wrzucony do esi w kolejnej iteracji
+
+	mov		ecx, [esp+4]			; ustaw limit dzialania 'repz cmpsb' na wielkosc slowa szukanego
+	cld								; ustawienie kierunku szukania do przodu 
+	repe		cmpsb				; porownuj bajty spod esi, z bajtami spod edi
+									; ciagi równe je¿eli CX=0	
+	test		ecx, ecx			; jesli zero, to wszystkie bajty spod edi pasowaly do bajtow spod esi - znaczy, ze znalezlismy ciag
+	jz		JEST					; jesli zero, to znaleziono
+	jmp		BRAK					; jesli nieznaleziono to skacz do BRAk
+JEST:
+	inc		eax						;zwieksz ilosc znaleziono o 1
+BRAK:
+	cmp 		ebx, edx			; porownaj czy osiagnieto koniec szukania w linii
+	jb		PETLA					;jump if Below (skocz do PETLI jesli nie koniec linii)
+
+	ret								;zwracamy wartosc z akumulatora czyli EAX
+AsmStart endp
+
+end
---
+++ WyszukiwarkaSlow/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
+using static System.Windows.Forms.LinkLabel;
+
+namespace WyszukiwarkaSlow
+{
+  
+
+    public partial class Form1 : Form
+    {
+
+        public class AsmProxy
+        {
+            [DllImport("AsmSzukaj.dll")]
+            private static extern int AsmStart(int mdlug_s, string mszuk, string mlancuch, int m_dlug_l);
+            public int executeAsmStart(int mdlug_s, string mszuk, string mlancuch, int m_dlug_l)
+            {
+                return AsmStart(mdlug_s, mszuk, mlancuch, m_dlug_l);
+            }
+        }
+
+        AsmProxy asmP = new AsmProxy();
+
+        private string fileContent;
+        private Stopwatch stopwatch;
+        private bool useCSharp = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "TekstDetektor";
+            textBox1.ReadOnly = true;
+            textBox3.ReadOnly = true;
+            textBoxTime.ReadOnly = true;
+            stopwatch = new Stopwatch();
+            InitializeComboBox();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Pliki tekstowe (*.txt)|*.txt|Wszystkie pliki (*.*)|*.*";
+            openFileDialog1.Title = "Wybierz plik tekstowy";
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Text = fileContent;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+
+                Task.Run(() =>
+                {
+                    fileContent = File.ReadAllText(filePath);
+                    Invoke(new Action(() => textBox1.Text = fileContent));
+                });
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            useCSharp = (comboBoxLanguage.SelectedItem.ToString() == "C#");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(fileContent))
+            {
+                MessageBox.Show("Nie załadowano pliku. Wybierz plik tekstowy przed zliczaniem wystąpień słowa.", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string searchedWord = textBox2.Text.Trim();
+
+            if (string.IsNullOrEmpty(searchedWord))
+            {
+                MessageBox.Show("Wprowadź słowo do zliczenia.");
+                return;
+            }
+
+            stopwatch.Restart();
+
+            int occurrences;
+
+            if (useCSharp)
+            {
+                occurrences = CountOccurrences(fileContent, searchedWord);
+            }
+            else
+            {
+                occurrences = CountOccurrencesAsm(fileContent, searchedWord);
+            }
+
+            stopwatch.Stop();
+
+            textBox3.Text = $"{occurrences}";
+            textBoxTime.Text = $"Czas zliczania: {stopwatch.ElapsedMilliseconds} ms";
+        }
+
+        private int CountOccurrencesAsm(string content, string searchedWord)
+        {
+            string linia;
+            int dlugosc_linii, do_szukania, dlugosc, jest, znaleziono;
+            StreamReader sr = new StreamReader(openFileDialog1.FileName);
+            linia = sr.ReadLine();
+            znaleziono = 0;
+            while (linia != null)
+            {
+                dlugosc = searchedWord.Length;
+                dlugosc_linii = linia.Length;
+                do_szukania = 0;
+               
+                if ((dlugosc_linii - dlugosc) >= 1)
+                {
+                    do_szukania = dlugosc_linii - dlugosc;
+                }
+                jest = 0;
+
+                if (do_szukania >= 1)
+                {
+                    jest = asmP.executeAsmStart(dlugosc, searchedWord, linia, do_szukania);
+                }
+                if (jest >= 1)
+                {
+                    znaleziono = znaleziono + jest;
+                   
+                }
+
+                linia = sr.ReadLine();
+            }
+
+            sr.Close();
+            return znaleziono;
+        }
+
+
+
+
+        private void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private int CountOccurrences(string content, string searchedWord)
+        {
+            string linia, podciag;
+            int dlugosc_linii, do_szukania, dlugosc, jest, znaleziono, pozycja;
+            StreamReader sr = new StreamReader(openFileDialog1.FileName);
+            linia = sr.ReadLine();
+           
+            znaleziono = 0;
+            while (linia != null)
+            {
+                
+                dlugosc = searchedWord.Length;
+                dlugosc_linii = linia.Length;
+                do_szukania = 0;
+                
+                if ((dlugosc_linii - dlugosc) >= 1)
+                {
+                    do_szukania = dlugosc_linii - dlugosc;
+                }
+                jest = 0;
+
+                // tu ASM - czy szukaj = podciag
+                if (do_szukania >= 1)
+                {
+                    pozycja = 0;
+                    dlugosc_linii = linia.Length;
+                 
+                    // to robi to samo co asembler
+                    while (pozycja <= (dlugosc_linii - dlugosc))
+                    {
+                        jest = 0;
+                        podciag = linia.Substring(pozycja, dlugosc);
+
+                        // tu ASM - czy szukaj = podciag
+
+                        if (podciag == searchedWord)
+                        { 
+                        jest = 1;
+                        }
+                        if (jest == 1)
+                        {
+                            znaleziono++;
+                            
+                            pozycja = pozycja + dlugosc ;
+                        }
+                        else
+                            pozycja = pozycja + 1;
+                    }
+
+                    
+                }
+                
+               
+                linia = sr.ReadLine();
+            }
+
+
+            
+            sr.Close();
+
+            return znaleziono;
+        }
+
+
+
+        private void InitializeComboBox()
+        {
+            comboBoxLanguage.Items.Add("C#");
+            comboBoxLanguage.Items.Add("Asembler");
+            comboBoxLanguage.SelectedIndex = 0;
+
+            comboBoxLanguage.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+        }
+
+        
+    }
+
+  
+}
